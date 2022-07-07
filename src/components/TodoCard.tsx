d3c2c4a861238be094269b25e3eb90ff1d35a494
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-07 08:12:15 +0800

    added react-icons

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Todo } from "../interfaces";
 import myStyles from "../utils/styles";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
 type Props = {
   todo: Todo;
@@ -13,18 +15,24 @@ const TodoCard = ({ todo, setTodos, todos }: Props) => {
     setTodos(todos.filter((t) => t.id !== tid));
   };
   return (
-    <div className="flex p-5 m-2 border-2 rounded-md place-content-center bg-gradient-to-tr from-slate-400 via-gray-200 to-slate-700 shadow-2xl">
-      <div className="">
-        <h1>Todo Name: {todo.todoName}</h1>
-        <h2>Todo Description : {todo.todoDescription}</h2>
-        <button
-          onClick={() => doneBtnHandler(todo.id)}
-          className={myStyles.button}
-        >
-          Done
-        </button>
+    <>
+      <div className="p-5 m-2 border-2 rounded-md place-content-center bg-gradient-to-tr from-slate-400 via-gray-200 to-slate-700 shadow-2xl">
+        <div className="grid grid-cols-2">
+          <h1>Todo Name: {todo.todoName}</h1>
+          <h2>Todo Description : {todo.todoDescription}</h2>
+          {/* <button
+            onClick={() => doneBtnHandler(todo.id)}
+            className={myStyles.button}
+          >
+            Done
+          </button> */}
+        </div>
+        <div className="flex w-full place-content-end">
+          <FaEdit size={28} />
+          <MdDeleteForever size={30} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
